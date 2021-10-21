cdb1312f7e710024e51bee8423b4ad4d9a670ba0
smallka <iamsmallka@163.com>
2021-10-21 16:26:53 +0800

    剩余参数的类型推导

--- learn-typing-function.ts
+++ learn-typing-function.ts
@@ -1,6 +1,8 @@
 export {}
 console.log("------------------ learn-typing-function ------------------")
 
+
+// ------------ 函数子类型
 type Mixed = number | string
 
 type Number2String = (v: number) => string
@@ -32,3 +34,25 @@ let good1: Number2String = mixed2String
 // 函数子类型的条件2：返回值必须是子类型
 let good2: Number2Mixed = number2String
 // WRONG let bad2: Number2String = number2Mixed
+
+// ---------- 剩余参数的类型推导
+function tuple<T extends unknown[]>(...ts: T) : T {
+	return ts
+}
+
+let a = [1, true]  // (number|boolean)[]
+let b = tuple(1, true)  // [number, boolean]
+
+function callFunction<T extends unknown[]>(
+    f: (...args: T) => void,
+    ...args: T
+): void {
+    f(...args)
+}
+
+function test(num: number, str: string) {
+    console.log(num + str)
+}
+callFunction(test, 2, "haha")
+// WRONG callFunction(test, 2)
+// WRONG callFunction(test, 2, 3)
