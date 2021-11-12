9b8c8aead91281fa6aff97fcfb00d66167b9df43
smallka <iamsmallka@163.com>
2021-11-12 10:36:24 +0800

    update hotfix-class

--- src/hotfix-class.ts
+++ src/hotfix-class.ts
@@ -1,20 +1,36 @@
 export {}
 console.log("------------------ hotfix-class ------------------")
 
-class A
+class Root
+{
+    public constructor()
+    {
+        this.initForHotfix()
+    }
+    public initForHotfix()
+    {
+        // wait subclass override for hotfix
+    }
+}
+
+class A extends Root
 {
     private static readonly zero = 0
     public val: number
     public constructor(val: number)
     {
+        super()
         this.val = val
+        console.log('A.constructor')
     }
     public add(other: number)
     {
+        console.log('call A.add');
         this.val += other
     }
     public static addition(p: number, q: number)
     {
+        console.log('call A.addition');
         return p + q
     }
     public static getZero()
@@ -23,10 +39,24 @@ class A
     }
     private innerAdd(other: number)
     {
+        console.log('call A.innerAdd');
         this.val += other
+    }
+    protected mul(other: number)
+    {
+        console.log('call A.mul');
+        this.val *= other
     }
 }
 
+console.log(Root.prototype)
+console.log(Object.getOwnPropertyNames(Root))
+console.log(Object.getOwnPropertyNames(Root.prototype))
+console.log(A.prototype)
+console.log(Object.getOwnPropertyNames(A))
+console.log(Object.getOwnPropertyNames(A.prototype))
+
+console.log('------ create A')
 let a = new A(10)
 a.add(3)
 console.log(a.val)
@@ -36,23 +66,30 @@ cb(2)
 console.log(a.val)
 console.log(`zero: ${A.getZero()}`)
 console.log(`addition: ${A.addition(6, 4)}`)
+console.log(a)
 
-console.log(A.prototype.add)
-
+console.log('------ start hotfix')
+// hotfix方法
 function add2(this: A, other: number)
 {
     this.val -= other
 }
-
-A.prototype.add = add2
+A.prototype.add = add2;
+// hotfix私有方法
+(<any>(A.prototype)).innerAdd = add2;
+// hotfix静态函数
 A.addition = (p: number, q: number) =>
 {
     return p - q
 }
-// 属性绕过private和const
+// hotfix静态属性，绕过private和const
 (<any>A).zero = 100;
-// 方法绕过private
-(<any>(A.prototype)).innerAdd = add2;
+// hotfix增加新变量
+function initForHotfix(this: A)
+{
+    (<any>this)['newVal'] = 255
+}
+A.prototype.initForHotfix = initForHotfix
 
 a.add(3)
 console.log(a.val)
@@ -60,6 +97,42 @@ cb(2)  // not working
 console.log(a.val)
 console.log(`zero: ${A.getZero()}`)
 console.log(`addition: ${A.addition(6, 4)}`)
-
 console.log(A.prototype.add)
 
+console.log(A === A.prototype.constructor)
+console.log(A.prototype)
+console.log(Object.getOwnPropertyNames(A))
+console.log(new A(2))
+
+class B extends A
+{
+    public mul(other: number)
+    {
+        console.log('call mul');
+        super.mul(other+1)
+    }
+}
+
+let b = new B(2)
+b.mul(5)
+console.log(b.val)
+
+function mul2(this: B, other: number)
+{
+    console.log('call mul2');
+    // 调用父类函数
+    (<any>(A.prototype)).mul.call(this, other)
+}
+// hotfix重载函数
+B.prototype.mul = mul2
+
+b.mul(5)
+console.log(b.val)
+
+console.log(A.prototype)
+console.log(Object.getOwnPropertyNames(A.prototype))
+for (let key of Object.getOwnPropertyNames(A.prototype))
+{
+    console.log(key)
+    console.log((<any>(A.prototype))[key])
+}
